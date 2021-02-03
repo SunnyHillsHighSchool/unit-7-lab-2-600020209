461754f5a4e2630a0388f836fc5c39d7a6873a2c
Joon Kyo Kyo <600020209@fjuhsd.org>
2021-02-03 04:33:07 +0000

    Completed go method within Class ListLast

--- ListLast.java
+++ ListLast.java
@@ -10,6 +10,17 @@ public class ListLast
 {
 	public static boolean go( List<Integer> ray)
 	{
-		return true;
+		//Begin for loop, with iterator i initialized with 0, while i is less than 1 less the length of the list, and with i incremented by one each run:
+    for(int i = 0; i < ray.size() - 1; i++){
+      //If the element of list at index i is equal to the element of list at index 1 less the length of list obtained with get methods:
+      if(ray.get(i) == ray.get(ray.size() - 1)){
+      //return true
+      return true;
+      //end if
+      }
+    //end for
+    }
+    //return false
+    return false;
 	}
 }
